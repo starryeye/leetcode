00eefe40f98e8ba8aa0499463e0bafea71a6bea7
starryeye <33487061+starryeye@users.noreply.github.com>
2022-03-26 06:13:50 +0900

    Time: 390 ms (5.23%), Space: 6.6 MB (92.85%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -7,6 +7,7 @@ public:
         
         dp[0] = 0;
         
+        //nums 배열의 값의 dp 는 1로 시작해야한다.
         for(int i=0; i<nums.size(); i++)
         {
             if(nums[i] > target)
@@ -17,7 +18,7 @@ public:
         
         for(int i=1; i<=target; i++)
         {
-            for(int j=0; j<i; j++)
+            for(int j=0; j<i; j++) //밑에 정답지를 보니.. j for문을 없앨수도 있다..
             {
                 for(int x=0; x<nums.size(); x++)
                 {
@@ -32,3 +33,25 @@ public:
         return dp[target];
     }
 };
+
+
+
+/*
+//정답
+int combinationSum4(vector<int>& nums, int target)
+{
+    vector<unsigned int> dp(target + 1, 0);
+    dp[0] = 1; //dp[0]에 1을 해놓으면 내가 해놓은 1 초기화 없어도됨..
+    
+    for (int i = 1; i <= target; ++i)
+    {
+        for (int x = 0; x < nums.size(); ++x)
+        {
+            if (i >= nums[x])
+                dp[i] += dp[i - nums[x]];
+        }
+    }
+    
+    return dp[target];
+}
+*/
