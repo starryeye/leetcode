f855590d70ab658d6892185371ec4b6eb216a364
starryeye <33487061+starryeye@users.noreply.github.com>
2022-03-30 03:08:48 +0900

    Time: 3 ms (52.66%), Space: 7.7 MB (76.02%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        
+        if(nums.size()==1)
+            return nums[0];
+        
+        vector<int>dp(nums.size());
+        
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        
+        for(int i=2;i<nums.size();i++)
+        {
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        
+        return dp[nums.size()-1];
+    }
+};
+
+/*
+class Solution {
+public:
+    int rob(vector<int>& nums) {
         //주어진 배열이 곧 dp 배열인듯.
         
         int n = nums.size();
@@ -27,3 +49,4 @@ public:
         return max(nums[n-1], nums[n-2]); //최댓값은 항상 맨마지막 또는 그 바로앞이다.
     }
 };
+*/
