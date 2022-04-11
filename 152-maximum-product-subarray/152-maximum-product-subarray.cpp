32efe832217d9e00bc4bb7423955a2bf307dcf6c
starryeye <33487061+starryeye@users.noreply.github.com>
2022-04-12 00:32:21 +0900

    Time: 8 ms (69.91%), Space: 14.3 MB (6.53%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -8,12 +8,13 @@ public:
         
         for(int i = 1; i < N; i++)
         {
-            dpmax[i] = max(nums[i], max(dpmax[i-1]*nums[i], dpmin[i-1]*nums[i]));
-            dpmin[i] = min(nums[i], min(dpmax[i-1]*nums[i], dpmin[i-1]*nums[i]));
+            //절대값으로 가장 큰 값들을 갱신한다.
+            dpmax[i] = max(nums[i], max(dpmax[i-1]*nums[i], dpmin[i-1]*nums[i])); //양수
+            dpmin[i] = min(nums[i], min(dpmax[i-1]*nums[i], dpmin[i-1]*nums[i])); //음수
             
             minmax = max(minmax, dpmin[i]);
             maxmax = max(maxmax, dpmax[i]);
         }
-        return max(minmax, maxmax);
+        return maxmax;
     }
 };
