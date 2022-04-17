5e6153375698701a5f57fff937814a0abd616a66
starryeye <33487061+starryeye@users.noreply.github.com>
2022-04-17 20:44:32 +0900

    Time: 15 ms (7.80%), Space: 12.1 MB (7.77%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -35,4 +35,28 @@ public:
         return comp;
         
     }
+    
+    /*
+    string longestCommonPrefix(vector<string>& str) {
+        int n = str.size();
+        if(n==0) return "";
+        
+        string ans  = "";
+        sort(begin(str), end(str)); //정렬을 하면 사전순으로 정렬됨.
+        string a = str[0];
+        string b = str[n-1]; // 마지막 문자열과 첫번째 문자열을 비교하여 달라지는 부분이 정답이다.
+        
+        for(int i=0; i<a.size(); i++){
+            if(a[i]==b[i]){
+                ans = ans + a[i];
+            }
+            else{
+                break;
+            }
+        }
+        
+        return ans;
+        
+    }
+    */
 };
