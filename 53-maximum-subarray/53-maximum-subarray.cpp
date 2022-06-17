60eff315ad1bf704d460f15c82502cdb15cff2dd
starryeye <33487061+starryeye@users.noreply.github.com>
2022-06-18 01:35:43 +0900

    Time: 256 ms (5.16%), Space: 70.4 MB (6.02%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        // dp[i] : 0 ~ i 인덱스의 sub array 중 i 인덱스를 포함하는 sub array들의 max sum
+        vector<int> dp(nums.begin(), nums.end());
+        
+        for(int i=1; i<nums.size(); i++) {
+            
+            // dp[i]가 max라는 말은 0이 max 값 즉, 그 전의 sub array는 음수이므로 버리겠다는 뜻이다.
+            dp[i] = max(dp[i], dp[i-1] + nums[i]);
+        }
+        
+        return *max_element(dp.begin(), dp.end());
+    }
+};
