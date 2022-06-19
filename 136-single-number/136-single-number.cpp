7d310a43fff5dce6916e8caeb9308049d5ae9166
starryeye <33487061+starryeye@users.noreply.github.com>
2022-06-19 15:21:20 +0900

    Time: 38 ms (31.23%), Space: 19.8 MB (20.81%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //O(n)
     int singleNumber(vector<int>& nums) {
         
         unordered_map<int, int> um;
@@ -19,4 +20,23 @@ public:
         return -1;
         
     }
+    
+    /*
+    //O(nlogn)
+    int singleNumber(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        
+        // 요소가 1개인 경우는 for문 진입 못함
+        for(int i=1; i < nums.size(); i+=2) {
+            if(nums[i-1] != nums[i])
+                return nums[i-1];
+        }
+        
+        //요소가 1개있는경우
+        //정렬후 마지막요소가 조건 만족하는 답일 경우
+        return nums[nums.size()-1];
+        
+    }
+    */
 };
