8d04d2c434b3aca61be53a145f62a37720fee290
starryeye <33487061+starryeye@users.noreply.github.com>
2022-06-20 23:19:54 +0900

    Time: 25 ms (66.28%), Space: 19.4 MB (92.13%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,9 +1,41 @@
 class Solution {
 public:
+    //sort
     int majorityElement(vector<int>& nums) {
         
         sort(nums.begin(), nums.end());
         
         return nums[nums.size() / 2];
     }
+    
+    /*
+    //hash
+    int majorityElement(vector<int>& nums) {
+        
+        unordered_map<int, int> um;
+        
+        for(auto num : nums) {
+            if(++um[num] > nums.size() / 2)
+                return num;
+        }
+        return -1;
+    }
+    
+    //moore vote
+    int majorityElement(vector<int>& nums) {
+        
+        int counter = 0;
+        int answer;
+        
+        for(auto num : nums) {
+            
+            if(!counter)
+                answer = num;
+            
+            counter += num == answer ? 1 : -1;
+        }
+        
+        return answer;
+    }
+    */
 };
