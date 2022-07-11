ef925dacce8ab9d45ab61fcfe29f68d35f3e1a04
starryeye <33487061+starryeye@users.noreply.github.com>
2022-07-11 19:33:24 +0900

    Time: 3 ms (81.77%), Space: 7.7 MB (55.11%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     vector<vector<int>> permute(vector<int>& nums) {
         
         vector<vector<int>> answer;
@@ -13,5 +14,31 @@ public:
         }while(next_permutation(nums.begin(), nums.end()));
         
         return answer;
+    }
+    */
+    
+    
+    vector<vector<int> > permute(vector<int> &nums) {
+	    vector<vector<int> > result;
+	    
+	    permuteRecursive(nums, 0, result);
+	    return result;
+    }
+    
+    // permute nums[begin..end]
+    // invariant: nums[0..begin-1] have been fixed/permuted
+	void permuteRecursive(vector<int> &nums, int begin, vector<vector<int> > &result)	{
+		if (begin >= nums.size()) {
+		    // one permutation instance
+		    result.push_back(nums);
+		    return;
+		}
+		
+		for (int i = begin; i < nums.size(); i++) {
+		    swap(nums[begin], nums[i]);
+		    permuteRecursive(nums, begin + 1, result);
+		    // reset
+		    swap(nums[begin], nums[i]);
+		}
     }
 };
