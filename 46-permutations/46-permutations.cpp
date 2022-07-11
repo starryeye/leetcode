d0812f6730316637316efd13d5cefa8baccd2923
starryeye <33487061+starryeye@users.noreply.github.com>
2022-07-11 19:28:36 +0900

    Time: 5 ms (50.32%), Space: 7.5 MB (91.87%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+        vector<vector<int>> answer;
+        sort(nums.begin(), nums.end());
+        
+        
+        do{
+            
+            answer.push_back(vector<int>(nums.begin(), nums.end()));    
+            
+        }while(next_permutation(nums.begin(), nums.end()));
+        
+        return answer;
+    }
+};
