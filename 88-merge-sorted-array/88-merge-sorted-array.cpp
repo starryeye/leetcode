3e8e1572e3a271bd39d791f0ed28d85de3b6f771
starryeye <33487061+starryeye@users.noreply.github.com>
2022-07-28 22:51:39 +0900

    Time: 0 ms (100.00%), Space: 9.1 MB (29.13%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     //Discuss에 더 나은 시간복잡도를 가진 solution이 있는듯 하다..
     //시간될때 보자..
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
@@ -11,5 +12,26 @@ public:
         
         sort(nums1.begin(), nums1.end(), less<int>());
         
+    }
+    */
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        int a = m - 1; //nums1 숫자 부분의 가장 마지막 index
+        int b = n - 1; //nums2 숫자 부분의 가장 마지막 index
+        int i = m + n - 1; //nums1의 마지막 index
+        
+        //비어있는, nums1의 뒷부분 부터 채운다.
+        while(a >= 0 && b >= 0) {
+            
+            if(nums1[a] > nums2[b])
+                nums1[i--] = nums1[a--];
+            else
+                nums1[i--] = nums2[b--];
+        }
+        
+        //남아있는 nums2 그대로 num1으로 채운다.
+        //nums1은.. 그대로 nums1이므로 copy 불필요
+        while(b >= 0)
+            nums1[i--] = nums2[b--];
     }
 };
