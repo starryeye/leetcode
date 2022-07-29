b26a41ac24350ef3984a8fd6e8eae9bd596b2151
starryeye <33487061+starryeye@users.noreply.github.com>
2022-07-29 23:22:11 +0900

    Time: 3 ms (62.21%), Space: 5.9 MB (63.51%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    /*
+    //hash map
     bool isHappy(int n) {
         
         unordered_map<int, int> um;
@@ -25,4 +27,36 @@ public:
         
         return true;
     }
+    */
+    //two pointers, floyd cycle detection
+protected:
+    int next(int n)
+    {
+        int sum = 0;
+        
+        while(n != 0)
+        {
+            sum += pow(n % 10,2);
+            n = n / 10;
+        }
+        
+        return sum;
+    }
+    
+public:
+    bool isHappy(int n) {
+        int slow = next(n);
+        int fast = next(next(n));
+        
+        //다르면 계속한다, 같아질때 까지...
+        while(slow != fast)
+        {
+            slow = next(slow);
+            fast = next(next(fast));
+        }
+        
+        //같아졌을때.. 그게 1인가..? 1이 아닌가...?
+        return fast == 1 ;
+    }
+    
 };
