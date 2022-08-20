85db04694b6272f02baff8ed055ecc5cc99beb03
starryeye <33487061+starryeye@users.noreply.github.com>
2022-08-21 02:00:30 +0900

    Time: 0 ms (100.00%), Space: 6.1 MB (8.70%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int fib(int n) {
+        
+        if(n == 0)
+            return 0;
+        else if(n == 1)
+            return 1;
+        
+        vector<int> dp(n+1, 0); //n이 index로 들어온다.
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i = 2; i < n+1; i++) {
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+        
+        return dp[n];
+    }
+};
