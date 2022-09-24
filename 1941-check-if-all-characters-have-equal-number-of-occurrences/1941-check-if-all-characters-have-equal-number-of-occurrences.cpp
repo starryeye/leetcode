aad22c807b0e23e842ccc609a169a3d4653f34ae
starryeye <33487061+starryeye@users.noreply.github.com>
2022-09-24 18:48:26 +0900

    Time: 13 ms (11.81%), Space: 6.8 MB (65.12%) - LeetHub

---
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool areOccurrencesEqual(string s) {
+        
+        unordered_map<char, int> um;
+        int tmp;
+        
+        for(int i = 0; i < s.size(); i++) {
+            um[s[i]]++;
+            tmp = um[s[i]];
+        }
+        
+
+        for(auto u : um) {
+            if(u.second != tmp)
+                return false;
+        }
+        
+        return true;
+    }
+};
