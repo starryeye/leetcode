b23e850950b26cccfd2068ac5c833348f022d974
starryeye <33487061+starryeye@users.noreply.github.com>
2022-10-24 21:18:01 +0900

    Time: 112 ms (23.16%), Space: 10.8 MB (15.31%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    //중복 제거
+    int firstUniqChar(string s) {
+        
+        unordered_map<char, pair<int, int>> um;
+        
+        for(int i = 0; i < s.size(); i++) {
+            um[s[i]].first++;
+            um[s[i]].second = i;
+        }
+        
+        int answer = s.size();
+        
+        for(auto u : um) {
+            if(u.second.first == 1)
+                answer = min(answer, u.second.second);
+        }
+        
+        return answer == s.size() ? -1 : answer;
+    }
+    
+    /*
     int firstUniqChar(string s) {
         
         unordered_map<char, int> um;
@@ -15,4 +36,5 @@ public:
         
         return -1;
     }
+    */
 };
