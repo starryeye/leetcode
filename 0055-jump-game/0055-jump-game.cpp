2c905965899277446f0ac19e1a810168872b2eb2
starryeye <33487061+starryeye@users.noreply.github.com>
2022-10-31 22:26:49 +0900

    Time: 69 ms (94.08%), Space: 48.4 MB (78.36%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    
+     bool canJump(vector<int>& nums) {
+         
+         int max_arrival_index = nums[0] + 0; //도달가능한 최대 사거리 + 최대 사거리의 출발 인덱스
+         
+         for(int i = 1; i < nums.size(); i++) {
+             
+             //현재의 능력으로는 도달 불가능한 인덱스이다..
+             if(max_arrival_index < i)
+                 return false;
+             
+             //최대 사거리 및 인덱스 갱신
+             if(max_arrival_index < nums[i] + i)
+                 max_arrival_index = nums[i] + i;
+         }
+         
+         return true;
+     }
+    
+    
+    /*
+    -> 마지막 인덱스 도달 경우의 수...
+    bool canJump(vector<int>& nums) {
+        
+        vector<int> dp(nums.size(), 0);
+        
+        dp[0] = 1;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            
+            for(int j = i + 1; j <= i + nums[i]; j++) {
+                
+                if(j > nums.size() - 1)
+                    continue;
+                
+                dp[j] += dp[i];
+            }
+        }
+        
+        return dp[nums.size() - 1] > 0;
+    }
+    */
+};
