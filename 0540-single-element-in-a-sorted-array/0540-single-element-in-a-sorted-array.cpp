68522cacca9bf25bbb2672b8ca721e9f743d56e4
starryeye <33487061+starryeye@users.noreply.github.com>
2022-11-02 22:39:28 +0900

    Time: 76 ms (26.08%), Space: 22.3 MB (91.46%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    // 0 ~ index + 1 까지 정상의 경우..
+    // -> index가 짝수면 무조건 index + 1과 값동일
+    // -> index가 홀수면 무조건 index - 1과 값동일
+    int singleNonDuplicate(vector<int>& nums) {
+        
+        int l = 0;
+        int r = nums.size() - 1;
+        
+        while(l < r) {
+            
+            int mid = l + (r - l)/2;
+            
+            if((mid % 2 == 0) && (nums[mid] == nums[mid + 1])) // 짝수
+                l = mid + 2;
+            else if((mid % 2 == 1) && (nums[mid] == nums[mid - 1])) // 홀수
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        
+        return nums[l];
+    }
+};
