d53fe95f789f5378d3429531ad671938530b2859
starryeye <33487061+starryeye@users.noreply.github.com>
2022-11-07 22:04:40 +0900

    Time: 792 ms (23.61%), Space: 144.3 MB (28.83%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        
+        int answer = nums.size() + 1;
+        
+        unordered_map<int, int> um; //k: prefix sum, v: count
+        
+        int prefix = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            prefix += nums[i];
+            
+            if(prefix > x) //목표 값 보다 커지면 그만!
+                break;
+            
+            um[prefix] = i + 1;
+            
+            if(prefix == x) //prefix만 사용해서 x가 될경우
+                answer = min(answer, i + 1);
+        }
+        
+        int suffix = 0;
+        for(int i = nums.size() - 1; i >= 0; i--) {
+            suffix += nums[i];
+            
+            if(suffix > x) //목표 값 보다 커지면 그만!
+                break;
+            
+            if(suffix == x) //suffix만 사용해서 x가 될경우
+                answer = min(answer, (int)nums.size() - i);
+            
+            if(um.find(x - suffix) != um.end())
+                answer = min(answer, ((int)nums.size() - i) + um[x - suffix]);
+        }
+        
+        return answer == nums.size() + 1 ? -1 : answer;
+    }
+};
