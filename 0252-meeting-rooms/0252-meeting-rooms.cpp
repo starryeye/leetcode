c97d0323795d85de1f1390f599a9bb5cedaff9fd
starryeye <33487061+starryeye@users.noreply.github.com>
2022-11-11 22:18:16 +0900

    Time: 55 ms (15.02%), Space: 14 MB (12.80%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.cpp
+++ 0252-meeting-rooms/0252-meeting-rooms.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+    /*
+    //Discuss에 람다를 사용한 풀이법이 많은데 왜 안될까..
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        
+        sort(intervals.begin(), intervals.end(), 
+            [](const Interval& interval1, const Interval& interval2){
+                return interval1.start < interval2.start;
+            });
+        
+        for (int i = 1; i < intervals.size(); i++){
+            
+            if (intervals[i].start < intervals[i-1].end)
+                return false;
+        }
+        
+        return true;
+    }
+    */
+    
     bool canAttendMeetings(vector<vector<int>>& intervals) {
         
         map<int, int> mp;
@@ -18,4 +37,5 @@ public:
         
         return true;
     }
+    
 };
