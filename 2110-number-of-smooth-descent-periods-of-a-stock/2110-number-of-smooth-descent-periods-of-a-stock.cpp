6e20cc03719f7b1eef5f22da9be1eb6ce34095b3
starryeye <33487061+starryeye@users.noreply.github.com>
2022-12-01 20:28:56 +0900

    Time: 362 ms (44.40%), Space: 101.3 MB (6.68%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //time O(N)
     long long getDescentPeriods(vector<int>& prices) {
         
         
@@ -15,8 +16,29 @@ public:
             answer += dp[i];
         }
         
-        //answer = accumulate(dp.begin(), dp.end(), 0);
+        //accumulate의 3번째 파라미터는 합의 초기 값이다.
+        //해당 파라미터의 타입으로 합이 계산 되므로..
+        //그냥 0으로 하면 int 타입의 범위를 벗어나면 쓰레기값..
+        //0LL로 해줘야 long long 타입으로 된다.
+        //answer = accumulate(dp.begin(), dp.end(), 0LL);
         
         return answer;
     }
+    
+    //space O(1)
+    /*
+    long long getDescentPeriods(vector<int>& prices) {
+        int dp = 1;
+        long long ans = 1;
+        for(int i = 1; i < prices.size(); i ++) {
+            if(prices[i] == prices[i - 1] - 1) {
+                dp ++;
+            } else {
+                dp = 1;
+            }
+            ans += dp;
+        }
+        return ans;
+    }
+    */
 };
