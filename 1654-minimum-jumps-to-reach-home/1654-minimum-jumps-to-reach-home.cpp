698c5e6c519f930c782f877aaee140a2eb7e4d7e
starryeye <33487061+starryeye@users.noreply.github.com>
2022-12-06 23:49:13 +0900

    Time: 30 ms (94.57%), Space: 23.1 MB (59.42%) - LeetHub

---
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
+    
+		unordered_set<int> forbi(forbidden.begin(), forbidden.end());
+    
+		vector<vector<int>> visited(2, vector<int>(6000));
+		// visited array has two states:
+		// visited[0][i] means the ith cell was reached from its left hand side jumping forward, 
+		// visited[1][i] means the ith cell was reached from its right hand side jumping backward. 
+    
+		queue<pair<int, bool>> que; 
+		// true means your last jump was backward jumping, you cannot jump backward in current step.
+		// false, otherwise.
+    
+		que.push({0,false});
+		visited[0][0] = 1;
+		visited[1][0] = 1;
+		int ans = 0;
+    
+		while(!que.empty()) {
+            
+			int len = que.size();
+            
+			while(len > 0) {
+                
+				len--;
+				int cur = que.front().first;
+				bool flag = que.front().second; //뒤로 두번 체크용
+                que.pop();
+                
+				if(cur == x)
+					return ans;
+                
+				
+                
+				int forward = cur + a;
+				int backward = cur - b;
+                
+				if(forward < 6000 && visited[0][forward] == 0 && !forbi.count(forward)) {
+                    
+					que.push({forward, false});
+					visited[0][forward] = 1;
+				}
+                
+				if(backward >= 0 && visited[1][backward] == 0 && !forbi.count(backward) && !flag) {
+                    
+					que.push({backward, true});
+					visited[1][backward] = 1;
+				}
+			}
+            
+			ans++;
+		}
+    
+		return -1;
+	}
+};
