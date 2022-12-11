beb44102c56723b9d924d015b561eab50a4267b1
starryeye <33487061+starryeye@users.noreply.github.com>
2022-12-11 22:53:55 +0900

    Time: 486 ms (32.91%), Space: 81.1 MB (24.61%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    
+    struct cmp {
+        bool operator()(pair<int, int> a, pair<int, int> b) {
+            return a.second < b.second;
+        }
+    };
+    // 이건 왜 안되는 거임..?
+    // static bool cmp(pair<int, int> a, pair<int, int> b) {
+    //     return a.second < b.second;
+    // }
+    
+    int minSetSize(vector<int>& arr) {
+        //priority_queue<pair<int, int>, cmp> pq;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
+        unordered_map<int, int> um;
+        
+        for(auto a : arr) {
+            um[a]++;
+        }
+        
+        for(auto u : um) {
+            pq.push(u);
+        }
+        
+        int answer = 0;
+        int count = 0;
+        int originSize = arr.size();
+        
+        while(!pq.empty()) {
+            pair<int, int> t = pq.top();
+            pq.pop();
+            
+            count += t.second;
+            answer++;
+            
+            if(count >= originSize / 2)
+                return answer;
+        }
+        
+        return 0;
+    }
+};
