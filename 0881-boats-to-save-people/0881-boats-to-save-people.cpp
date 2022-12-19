588e0059104dd01d6ca19e45086994e93e301d09
starryeye <33487061+starryeye@users.noreply.github.com>
2022-12-19 21:49:46 +0900

    Time: 80 ms (98.03%), Space: 42 MB (56.13%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        
+        int answer = 0;
+        
+        sort(people.begin(), people.end());
+        
+        int l = 0;
+        int r = people.size() - 1;
+        
+        //내가 정답을 맞추기 위해 count를 썻지만.. 왜 써야 정답인지는 분석 필요..
+        int count = 0;
+        
+        while(l < r) {
+            
+            if(people[l] + people[r] > limit) {
+                
+                answer++;
+                r--;
+                count++;
+            }else {
+                
+                answer++;
+                r--;
+                l++;
+                count += 2;
+            }
+        }
+        
+        //answer에 포함되지 않은 사람들을 더해줌..
+        return answer + people.size() - count;
+    }
+};
