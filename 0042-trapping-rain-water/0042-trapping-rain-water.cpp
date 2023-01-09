c8206191b412ad1a75d859978b9a4936590f8f03
starryeye <33487061+starryeye@users.noreply.github.com>
2023-01-09 22:39:20 +0900

    Time: 17 ms (79.68%), Space: 20.6 MB (26.06%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,5 +1,30 @@
 class Solution {
 public:
+    
+    //직관적 풀이법
+    int trap(vector<int>& height) {
+        vector<int> lMax(height.size());
+        vector<int> rMax(height.size());
+
+        int mL = 0;
+        for(int i = 0;i<lMax.size();i++){
+            mL=max(height[i], mL);
+            lMax[i] = mL;
+        }
+        mL = 0;
+        for(int i = lMax.size()-1;i>-1;i--){
+            mL=max(height[i], mL);
+            rMax[i] = mL;
+        }
+        
+        int ans = 0;
+        for(int i = 0;i<height.size();i++){
+            ans += min(lMax[i],rMax[i])-height[i];
+        }
+        return ans;
+    }
+    
+    /*
     //아래 discuss, solution 2 참조
     //https://leetcode.com/problems/trapping-rain-water/discuss/1374608/C%2B%2BJavaPython-MaxLeft-MaxRight-so-far-with-Picture-O(1)-space-Clean-and-Concise
     
@@ -44,4 +69,5 @@ public:
         
         return ans;
     }
+    */
 };
