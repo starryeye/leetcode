b5b470d0ad7de518a065f9d6c168f5d6db917965
starryeye <33487061+starryeye@users.noreply.github.com>
2023-04-14 16:12:15 +0900

    Time: 972 ms (62.55%), Space: 0B (100.00%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
+# left outer join. == left join
+# inner join == join
 select c.name as Customers
 from Customers as c
 left outer join Orders as o on c.id = o.customerId
