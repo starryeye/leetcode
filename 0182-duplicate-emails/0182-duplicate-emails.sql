58c1f14fa009143d5d21e3225fa7d719e6fedc17
starryeye <33487061+starryeye@users.noreply.github.com>
2023-04-24 12:55:25 +0900

    Time: 778 ms (35.04%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,8 +1,17 @@
 # Write your MySQL query statement below
 
-select distinct(p1.email) as Email
-from Person as p1
-join Person as p2 
-    on p1.email = p2.email and 
-        p1.id <> p2.id;
+# select distinct(p1.email) as Email
+# from Person as p1 #as 생략 가능한듯
+# join Person as p2 #as 생략 가능한듯
+#     on p1.email = p2.email and 
+#         p1.id <> p2.id;
+
+select email
+from Person
+group by email
+having count(email) > 1;
+
+# select p1.email
+# from Persion p1, Person p2
+# where p1.email = p2.email and p1.id <> p2.id;
 
