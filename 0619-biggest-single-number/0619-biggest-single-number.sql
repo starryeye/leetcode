f4e623a719b4cac5c721c85d280449500f3d31fb
starryeye <33487061+starryeye@users.noreply.github.com>
2023-05-02 12:43:06 +0900

    Time: 798 ms (47.33%), Space: 0B (100.00%) - LeetHub

---
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+select
+    case
+        when count(num) = 1
+            then num
+        else
+            null
+    end as num
+from MyNumbers
+group by num
+order by num desc
+limit 1;
