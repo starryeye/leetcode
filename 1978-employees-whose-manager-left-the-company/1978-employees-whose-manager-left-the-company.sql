28f02d256bf2dac16840024fbacffb1b7ddbf0de
starryeye <33487061+starryeye@users.noreply.github.com>
2023-06-23 23:29:50 +0900

    Time: 2602 ms (5.02%), Space: 0B (100.00%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -7,3 +7,25 @@ where
     and manager_id is not null
     and salary < 30000
 order by employee_id asc;
+
+
+# discuss faster solution .. 
+# where 조건 순서가 중요하네...
+# SELECT employee_id
+# FROM Employees
+# WHERE salary < 30000
+# AND manager_id IS NOT NULL
+# AND manager_id NOT IN (SELECT employee_id FROM Employees)
+# ORDER BY employee_id;
+
+
+# discuss using self join
+# SELECT DISTINCT emp.employee_id
+# FROM   employees emp
+#        LEFT JOIN employees mng
+#               ON( emp.manager_id = mng.employee_id )
+# WHERE  emp.manager_id IS NOT NULL
+#        AND emp.salary < 30000
+#        AND mng.employee_id IS NULL
+# ORDER  BY emp.employee_id 
+
