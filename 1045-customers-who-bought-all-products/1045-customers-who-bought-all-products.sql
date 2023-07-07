fc0528d035042a4efb5da109e01b2438102897d0
starryeye <33487061+starryeye@users.noreply.github.com>
2023-07-07 23:32:21 +0900

    Time: 1037 ms (72.87%), Space: 0B (100.00%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,7 +1,10 @@
 # Write your MySQL query statement below
 
-
+# group by customer_id : customer_id 로 묶겠다.
+# having XXX : group by 를 진행한 결과에 조건 XXX 에 부합하는 애들만 보겠다는 의미이다.
+# 여기서는 count(distinct product_key) 와 select count(distinct product_key) from product 가 동일한 조건을 걸었다. 
 select customer_id
-from customer c
+from customer
 group by customer_id
-having count(distinct product_key)=(select count(distinct product_key) from product)
+having 
+    count(distinct product_key) = (select count(distinct product_key) from product)
