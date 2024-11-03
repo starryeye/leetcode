7f01433eef726f699e5975e6e9a0ba08fdf74dc0
starryeye <33487061+starryeye@users.noreply.github.com>
2024-11-03 20:11:17 +0900

    Time: 41 ms (18.32%), Space: 30.5 MB (12.64%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-
+/*
     // 123. Best Time to Buy and Sell Stock III 과 동일함.
 
-    // dp[k][i] : 현재, k 번째 거래완료, i index 날까지 고려
+    // dp[k][i] : 현재, k 번째 거래완료, i index 날까지 고려 최대 이익
     int maxProfit(int k, vector<int>& prices) {
         
         int n = prices.size();
@@ -22,7 +22,7 @@ public:
 
         return dp[k][n - 1];
     }
-
+*/
     /*
     // 1차원 배열로 최적화 가능..
     int maxProfit(int k, vector<int>& prices) {
@@ -45,4 +45,30 @@ public:
         return dp[n - 1];
     }
     */
+
+    int maxProfit(int k, vector<int>& prices) {
+
+        int n = prices.size();
+
+        // dp[k][i][0] : k번 거래 완료, i index 날까지 고려, 주식 보유X 최대 이익
+        // dp[k][i][1] : k번 거래 완료, i index 날까지 고려, 주식 보유O 최대 이익
+        vector<vector<vector<int>>> dp(k + 1, vector<vector<int>>(n, vector<int>(2)));
+
+        // 초기화
+        for (int g = 0; g <= k; g++) {
+            dp[g][0][0] = 0; // 0 index 에 주식 보유 X -> 이익 0
+            dp[g][0][1] = -prices[0]; // 0 index 에 주식 보유 O -> 이익 -prices[0]
+        }
+        
+        // 점화
+        // for 문의 순서는 상관 없는듯?
+        for (int g = 1; g <= k; g++) {
+            for (int i = 1; i < n; i++) {
+                dp[g][i][0] = max(dp[g][i - 1][0], dp[g][i - 1][1] + prices[i]); // 아무것도 안함 or 매도
+                dp[g][i][1] = max(dp[g][i - 1][1], dp[g - 1][i - 1][0] - prices[i]); // 아무것도 안함 or 매수
+            }
+        }
+
+        return dp[k][n-1][0];
+    }
 };
