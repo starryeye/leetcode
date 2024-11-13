9430cd109ce273b59c254a7b9a81bc2f46c6dc19
starryeye <33487061+starryeye@users.noreply.github.com>
2024-11-14 02:24:03 +0900

    Time: 0 ms (100%), Space: 13 MB (83.07%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+    int lengthOfLIS(vector<int>& nums) {
+
+        int n = nums.size();
+
+        vector<int> temp;
+        
+        for (int i = 0; i < n; i++) {
+
+            if (temp.empty() || temp.back() < nums[i]) {
+                temp.push_back(nums[i]);
+            } else {
+                auto indx = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin(); 
+                // Find the index of the first element >= nums[i] (이진탐색, temp 는 정렬되어있음)
+                
+                temp[indx] = nums[i]; 
+                // Replace that number with nums[i]
+            }
+        }
+        return temp.size();
+    }
+
+/*
     // dp[i]
     //      상태 : i index 를 마지막 요소로 포함하는 부분 배열
     //      값 : 요소 개수(길이) 최대값
@@ -25,7 +47,7 @@ public:
 
         return *max_element(dp.begin(), dp.end());
     }
-
+*/
 /*
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
