1ae2dd9735c7b56ac1013aa91aa176a0b8011e4a
starryeye <33487061+starryeye@users.noreply.github.com>
2024-11-16 21:18:59 +0900

    Time: 27 ms (14.09%), Space: 16.7 MB (93.78%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
-    // dp[i][j][0]
-    //      상태 : (i, j) 좌표를 오른쪽 아래 꼭짓점으로 하는 직사각형
-    //      값 : 가로 최대 크기
-    // dp[i][j][1]
-    //      상태 : (i, j) 좌표를 오른쪽 아래 꼭짓점으로 하는 직사각형
-    //      값 : 세로 최대 크기
+
     int maximalRectangle(vector<vector<char>>& matrix) {
-        if (matrix.empty()) return 0;
+	    
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        vector<vector<int>> memo(n, vector<int>(m, 0));
+	    
+        for (int j = 0; j < m; j++) {
+            if(matrix[0][j] == '1') {
+                memo[0][j] = 1;
+            }
+        }
 
-        int n = matrix.size();    // 행 개수
-        int m = matrix[0].size(); // 열 개수
-        int maxArea = 0;
-
-        // DP 배열 초기화
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(2, 0))); // 3차원 DP 배열
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == '1') {
-                    // 가로 크기 갱신
-                    dp[i][j][0] = (j == 0 ? 1 : dp[i][j - 1][0] + 1);
-
-                    // 세로 크기 갱신
-                    dp[i][j][1] = (i == 0 ? 1 : dp[i - 1][j][1] + 1);
-
-                    // 최대 면적 계산
-                    int minWidth = dp[i][j][0];
-                    for (int k = i; k >= 0; k--) { // 위로 탐색
-                        minWidth = min(minWidth, dp[k][j][0]); // 최소 가로 길이 갱신
-                        int area = minWidth * (i - k + 1);    // 직사각형 면적 계산
-                        maxArea = max(maxArea, area);        // 최대 면적 갱신
-                    }
+        //finding height of  each cloumn in each row.
+   	    for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if(matrix[i][j] == '1') {
+                    memo[i][j] = 1 + memo[i - 1][j];
                 }
             }
         }
 
-        return maxArea;
+	    int ret = 0;
+	    for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < m; ++j) {
+
+                int h = memo[i][j];
+                for(int k = j; k < m; ++k){
+				    h = min(h, memo[i][k]);
+				    ret = max( ret, (k - j + 1) * h);
+			    }
+            }
+        }
+		    
+	    return ret;
     }
 };
