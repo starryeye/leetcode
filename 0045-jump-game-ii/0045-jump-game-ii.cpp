2c3776b8a4fe6402ef52de7062bd07d55760cfa5
starryeye <33487061+starryeye@users.noreply.github.com>
2024-11-19 23:09:47 +0900

    Time: 64 ms (23.52%), Space: 20 MB (15.18%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    // 정수 배열 nums가 주어짐.
+    //      각 요소는 해당 위치에서의 최대 점프 길이를 나타냅니다.
+    //          반드시 최대로 점프안해도 됨.
+    // 처음에는 배열의 첫 번째 인덱스에 위치
+
+    // 마지막 인덱스에 도달가능하도록 배열이 주어진다.
+    // 마지막 인덱스에 도달하기 위한 최소의 점프 횟수는?
+
+    // dp[i]
+    //      상태 : i index 에 위치
+    //      값 : 끝 인덱스에 도달하기 위한 최소의 점프 횟수
+    int jump(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        vector<int> dp(n, INT_MAX);
+
+        // 초기화
+        dp[n - 1] = 0; // 끝인덱스는 도달 하기 위해 0 번의 점프 필요
+        
+        for (int currentIdx = n - 2; currentIdx >= 0; currentIdx--) {
+            
+            int maxReachableIdx = min(currentIdx + nums[currentIdx], n - 1);
+
+            for (int i = currentIdx; i <= maxReachableIdx; i++) {
+                if (dp[i] != INT_MAX) {
+                    dp[currentIdx] = min(dp[currentIdx], dp[i] + 1);
+                }
+            }
+        }
+
+        return dp[0]; 
+    }
+};
