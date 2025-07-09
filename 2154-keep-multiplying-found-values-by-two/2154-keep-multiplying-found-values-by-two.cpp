35b68a21aa8534be43eadea57ba418b87ceaf25d
starryeye <33487061+starryeye@users.noreply.github.com>
2025-07-10 00:37:26 +0900

    Time: 0 ms (100%), Space: 14.1 MB (69.05%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
+
+    // 전체 시간 복잡도가 O(n) 로 보이지만, 해시 충돌이 일어나면, O(n^2) 일어날 수도?
+    // int findFinalValue(vector<int>& nums, int original) {
+        
+    //     unordered_set<int> us;
+    //     for (int& n : nums) {
+    //         us.insert(n);
+    //     }
+
+    //     while(us.find(original) != us.end()) {
+    //         original = original * 2;
+    //     }
+
+    //     return original;
+    // }
+
     int findFinalValue(vector<int>& nums, int original) {
         
-        unordered_set<int> us;
-        for (int& n : nums) {
-            us.insert(n);
-        }
+        sort(nums.begin(), nums.end());
 
-        while(us.find(original) != us.end()) {
-            original = original * 2;
+        while(binary_search(nums.begin(), nums.end(), original)) {
+            original *= 2;
         }
 
         return original;
